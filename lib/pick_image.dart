cfa67e7cdc030401210d79cc89b235455491dd11
murhussain <husseinmurashi@yahoo.com>
2022-07-04 08:33:46 +0200

    adding textField with decorator

--- lib/pick_image.dart
+++ lib/pick_image.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ImagePickerExample extends StatefulWidget {
@@ -13,6 +14,8 @@ class ImagePickerExample extends StatefulWidget {
 class _ImagePickerExampleState extends State<ImagePickerExample> {
   File? _imageFile;
   final _picker = ImagePicker();
+
+  final TextEditingController pathController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -33,26 +36,53 @@ class _ImagePickerExampleState extends State<ImagePickerExample> {
               ),
             ],
           ),
-          if (this._imageFile == null)
-            const Placeholder()
+          if (_imageFile == null)
+            const Text('Please select image')
           else
-            Image.file(this._imageFile!),
+            // Image.file(_imageFile!),
+            // Text(_imageFile.toString())
+            TextField(
+              controller: pathController,
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                prefixIcon: IconButton(
+                  onPressed: () async {
+                    File? posLong = await _pickImageFromGallery1();
+                    setState(() {
+                      pathController.text = posLong.toString();
+                    });
+                  },
+                  icon: const Icon(Icons.photo_camera),
+                  color: Colors.red,
+                ),
+              ),
+            ),
         ],
       ),
     );
   }
 
+  
+
+  Future<File?> _pickImageFromGallery1() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() => _imageFile = File(pickedFile.path));
+    }
+    return _imageFile;
+  }
+
   Future<void> _pickImageFromGallery() async {
     final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
     if (pickedFile != null) {
-      setState(() => this._imageFile = File(pickedFile.path));
+      setState(() => _imageFile = File(pickedFile.path));
     }
   }
 
   Future<void> _pickImageFromCamera() async {
     final pickedFile = await _picker.pickImage(source: ImageSource.camera);
     if (pickedFile != null) {
-      setState(() => this._imageFile = File(pickedFile.path));
+      setState(() => _imageFile = File(pickedFile.path));
     }
     // print("ITS ME: $_imageFile");
   }
